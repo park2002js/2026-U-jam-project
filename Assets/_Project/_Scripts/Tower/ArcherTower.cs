707dd0ceea47aefa54275df4d289a823cabe039f
Seo132 <104410439+Seo132@users.noreply.github.com>
2026-04-28 15:16:04 +0900

    ray 시각화 및 탐지

--- Assets/_Project/_Scripts/Tower/ArcherTower.cs
+++ Assets/_Project/_Scripts/Tower/ArcherTower.cs
@@ -53,6 +53,24 @@ public class ArcherTower : DefenseBuilding
             }
         }
 
+        if (currentTarget != nearestEnemy)
+        {
+            // 1. 타겟이 없었는데 새로 발견했을 때
+            if (currentTarget == null && nearestEnemy != null)
+            {
+                Debug.Log($" 새로운 타겟 포착: '{nearestEnemy.name}'");
+            }
+            // 2. 타겟이 있었는데 사라졌을 때 (죽었거나 사거리 밖으로 도망침)
+            else if (currentTarget != null && nearestEnemy == null)
+            {
+                Debug.Log($"타겟 상실 (처치됨 또는 사거리 이탈)");
+            }
+            // 3. 둘 다 있는데 더 가까운 다른 타겟으로 변경되었을 때
+            else if (currentTarget != null && nearestEnemy != null)
+            {
+                Debug.Log($"타겟 변경: '{currentTarget.name}' -> '{nearestEnemy.name}'");
+            }
+        }
         currentTarget = nearestEnemy;
     }
 
@@ -66,6 +84,7 @@ public class ArcherTower : DefenseBuilding
             
             // ✨ 1. 타워와 적 사이의 거리를 계산합니다.
             float distance = Vector3.Distance(firePoint.position, targetCenter);
+            Debug.DrawRay(firePoint.position, direction * attackRange, Color.red, 2f);
 
             if (Physics.Raycast(firePoint.position, direction, out RaycastHit hit, attackRange, enemyLayer))
             {
@@ -104,7 +123,7 @@ public class ArcherTower : DefenseBuilding
         if (enemy != null)
         {
             enemy.takeDamage(attackDamage);
-            Debug.Log($"피격 후 {delay}초 경과 데미지 적용 완료.");
+            Debug.Log($"{delay}초 경과 데미지 적용 완료.");
         }
     }
 
