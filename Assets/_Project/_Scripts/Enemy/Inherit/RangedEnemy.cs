347a48852fc378845f66b516f184918b6c575fd5
park2002js <60747783+park2002js@users.noreply.github.com>
2026-08-12 11:40:15 +0900

    Enemy 수정 4
    투사체의 도착 좌표 위치를 보정함

--- Assets/_Project/_Scripts/Enemy/Inherit/RangedEnemy.cs
+++ Assets/_Project/_Scripts/Enemy/Inherit/RangedEnemy.cs
@@ -1,5 +1,6 @@
 using UJam.Runtime.Combat;
 using UJam.Runtime.Enemy.Projectiles;
+using UJam.Runtime.Grid;
 using UnityEngine;
 
 namespace UJam.Runtime.Enemy
@@ -14,7 +15,6 @@ namespace UJam.Runtime.Enemy
         // Projectile.cs를 컴포넌트로 보유한, Prefab화 된 날려보낼 투사체
         [SerializeField] private Projectile _projectilePrefab;
 
-        // 
         [SerializeField] private Transform _firePoint;
         [SerializeField] private ProjectileMovement _projectileMovement;
         [SerializeField, Min(0.01f)] private float _projectileSpeed = 5f;
@@ -47,6 +47,16 @@ namespace UJam.Runtime.Enemy
             }
 
             Vector3 destination = target.transform.position;
+            destination.y = 1f;
+
+            // 공격 대상이 거점이면, 투사체가 도달할 x축 좌표를 Enemy가 위치한 좌표로 고정하고, Z축 좌표는 거점이 위치한 Row 줄로 설정한다.
+            if (target.CompareTag("BaseCore"))
+            {
+                GridSystem grid = GridSystem.Instance;
+                destination.x = transform.position.x;
+                destination.z = grid.Origin.z + grid.BaseCoreRow * grid.CellHeight;
+            }
+
             Vector3 direction = destination - _firePoint.position;
             Quaternion rotation = direction.sqrMagnitude > 0f
                 ? Quaternion.LookRotation(direction)
